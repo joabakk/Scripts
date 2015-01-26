0225abc91789240e132e8841d6627491c06c0c8c
joabakk <github@heiamoss.com>
2015-01-26 11:07:12 +0100

    Update pynmea_test4.py
    split to be developed further

--- python/pynmea_test4.py
+++ python/pynmea_test4.py
@@ -3,6 +3,10 @@ with open("/home/pi/kplexlogs/example.log", "r") as fo:
   for line in fo:
         #print "Read: %s" % (line)
         #todo: strip off any timestamp/source tag
+        #split(str="", num=string.count(str))
+  |     #Splits string according to delimiter str (space if not provided) and returns list of substrings; split into at most num substrings if given
+        #tags = split(line="//")
+        #print tags
         msg = pynmea2.parse(line)
         print repr(msg)
         #print msg.type
