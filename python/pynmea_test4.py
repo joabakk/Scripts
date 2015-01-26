62c4889a116ecab78bd12adefd72e7abc74631ab
joabakk <github@heiamoss.com>
2015-01-26 08:04:49 +0100

    Update pynmea_test4.py
    trying to find talker type to extract right info

--- python/pynmea_test4.py
+++ python/pynmea_test4.py
@@ -1,14 +1,18 @@
 import pynmea2
 with open("/home/pi/kplexlogs/example.log", "r") as fo:
   for line in fo:
-        print "Read: %s" % (line)
+        #print "Read: %s" % (line)
         msg = pynmea2.parse(line)
         print repr(msg)
-        try:
-                print msg.rate_of_turn
-        except:
-                pass
-        try:
-                print msg.latitude
-        except:
-                pass
+        #print msg.type
+        #print msg.NMEASentenceType
+        #print msg.cls
+        #print msg.TalkerSentence
+        #try:
+        #        print msg.rate_of_turn
+        #except:
+        #        pass
+        #try:
+        #        print msg.latitude
+        #except:
+        #        pass
